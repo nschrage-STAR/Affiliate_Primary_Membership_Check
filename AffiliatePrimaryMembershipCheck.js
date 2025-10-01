4f80018f191ff5669cf22830e9e55672f3709080
nschrage-STAR <nschrage@suncoasttamparealtors.org>
2025-10-01 11:00:06 -0400

    Update AffiliatePrimaryMembershipCheck.js
    1.0.0.1

--- AffiliatePrimaryMembershipCheck.js
+++ AffiliatePrimaryMembershipCheck.js
@@ -1,7 +1,8 @@
 // JavaScript source code
 var req = new XMLHttpRequest();
-req.open("GET", Xrm.Page.context.getClientUrl() + "/api/data/v8.2/cobalt_memberships?$filter=_cobalt_membertypeid_value eq CA0B84B0-EF20-E111-B470-00155D000140 and  statecode eq eq and  _cobalt_accountid_value eq  and  ramcosub_subclass eq eq or  ramcosub_subclass eq eq or  ramcosub_subclass eq eq or  ramcosub_subclass eq eq or  ramcosub_subclass eq eq or  ramcosub_subclass eq eq", true);
-req.setRequestHeader("OData-MaxVersion", "4.0");
+var formContext = executionContext.getFormContext();
+var AccountGuid = formContext.data.entity.getId();
+req.open("GET", Xrm.Page.context.getClientUrl() + "/api/data/v8.2/cobalt_memberships?$filter=_ramco_officeid_value eq {AccountGuid} and (ramcosub_subclass eq 740300096 or ramcosub_subclass eq 740300033 or ramcosub_subclass eq 740300004 or ramcosub_subclass eq 740300005 or ramcosub_subclass eq 740300007) and statecode eq 0", true); req.setRequestHeader("OData-MaxVersion", "4.0");
 req.setRequestHeader("OData-Version", "4.0");
 req.setRequestHeader("Accept", "application/json");
 req.setRequestHeader("Content-Type", "application/json; charset=utf-8");
@@ -11,15 +12,9 @@ req.onreadystatechange = function () {
         req.onreadystatechange = null;
         if (this.status === 200) {
             var results = JSON.parse(this.response);
-            for (var i = 0; i < results.value.length; i++) {
-                var cobalt_membershipid = results.value[i]["cobalt_membershipid"];
-                function checkMemberValue(executionContext) {
-                    var formContext = executionContext.getFormContext();
-                    var memberValue = formContext.getAttribute("cobalt_membershipid").getValue();
-
-                    if (memberValue === null || memberValue === "" || memberValue === undefined) {
-                        alert("Primary Member value cannot be blank. Please enter a value.");
-                    }
+            if (results.value.length == 0) {
+                alert("Primary Member value cannot be blank. Please enter a value.");
+            }
                 }
 
             }
